6b0e6ac744aef3e6cb661bb996aade7b69ce8ff5
johnfn <johnfn@gmail.com>
2021-04-23 11:54:48 -0700

    Add autoreloading

--- src/Test.ts
+++ src/Test.ts
@@ -3,7 +3,7 @@ export class TestClass extends Node2D {
   constructor() {
     super()
 
-    this.get_node_safe("/root/Node2D/Label").text = "This is a game!"
+    this.get_node_safe("/root/Node2D/Label").text = "This is a game!!owoo"
   }
 }
 
