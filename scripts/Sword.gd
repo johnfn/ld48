e7df54a13f3aefe037723f16cdae0d716f4baffd
johnfn <johnfn@gmail.com>
2021-04-24 17:25:14 -0700

    Witches dont take dmg from swords on the ground

--- scripts/Sword.gd
+++ scripts/Sword.gd
@@ -4,18 +4,24 @@ onready var AnimationPlayer = $AnimationPlayer
 onready var swing_animation = AnimationPlayer.get_animation("Swing")
 var damage = 1
 var player: Player
+var is_in_use = false
 
 func _ready() -> void:
     self.connect("body_entered", self, "on_enter")
 
 func on_enter(other) -> void:
-    if other.has_method("is_enemy") and other.is_enemy():
-      other.damage(damage, self)
+  if not is_in_use:
+    return
+    
+  if other.has_method("is_enemy") and other.is_enemy():
+    other.damage(damage, self)
 
 func init(player: Player):
   self.player = player
 
 func set_in_use(in_use: bool) -> void:
+  self.is_in_use = in_use
+  
   if in_use:
     if not AnimationPlayer.is_playing():
       swing_animation.loop = true
