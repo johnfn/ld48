2a4604b465bd260da674800758257f19399b51ff
Mike Precup <mike@precup.email>
2021-04-26 15:58:57 -0700

    Main menu updates

--- levels/MainMenu.gd
+++ levels/MainMenu.gd
@@ -1,13 +1,10 @@
 extends Control
 
-var max_lean = 40
-var mouse_max = 300
 func _process(delta):
-  var mouse_vec = get_global_mouse_position() - Vector2(640, 640)
-  var strength = min(mouse_max, mouse_vec.length()) / mouse_max * max_lean
-  var pos = strength * mouse_vec.normalized()
+  var mouse_vec = get_global_mouse_position()
+  var pos = mouse_vec / 40
   $Title.margin_left = pos.x
-  $Title.margin_top = pos.y
+  $Title.margin_top = sqrt(pos.y) * 2
 
 
 func _on_Quit_pressed():
@@ -24,3 +21,7 @@ func _on_VolumeBar_value_changed(value):
 
 func _on_Credits_pressed():
   get_tree().change_scene("res://Credits.tscn")
+
+
+func _on_Volume_pressed():
+  $VolumeBar.visible = not $VolumeBar.visible
