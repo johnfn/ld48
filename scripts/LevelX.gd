1a76bfee2a815c73039b5f34544002f77ef2e5e6
Mike Precup <mike@precup.email>
2021-04-26 17:53:21 -0700

    Updated music for all levels

--- scripts/LevelX.gd
+++ scripts/LevelX.gd
@@ -5,6 +5,12 @@ export(float) var top_wall = 0;
 export(float) var bottom_wall = 0;
 export(bool) var is_top_open = true;
 export(bool) var dirty = false
+export(String) var song = "Chapter1Song"
+
+
+func get_song():
+  return song
+
 
 func _ready():
   if spawn_point == Vector2(0, 0):
